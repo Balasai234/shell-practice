646c146a34396b519039149fdeb3a4a69fca00e7
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-05-18 14:16:55 +0530

    Arthametic Operations

--- 04-Varables.sh
+++ 04-Varables.sh
@@ -5,3 +5,8 @@ Number2=$2
 Sum=$((Number1+Number2))
 
 echo "The sum of $Number1 and $ Number2 is:: $Sum"
+
+Number3=20
+Number4=10
+Multiplication=$((Number3*Number4))
+echo "The Multiplication of $Number3 and $Number4 is $Multiplication"
