e9eab4663bc9a082cfc18fc9b5752babe83ab6ea
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-05-18 20:22:22 +0530

    special vars

--- spec-var.sh
+++ spec-var.sh
@@ -2,3 +2,8 @@
 
 echo "All variables passed in the script $@"
 echo "Number of variables passed in the script $#"
+echo "Scriptname $0"
+echo "current directory $PWD"
+echo "User directory $USER"
+echo "Home directory $HOME"
+echo "Process id $$"
