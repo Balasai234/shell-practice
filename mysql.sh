5d76c9ad782a0dcb3433c19f57697a5c8048f080
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-05-18 22:40:29 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -4,6 +4,22 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "You are not running with root user access.."
+exit 1
 else
 echo "You are running with root user access"
+fi
+dnf  list installed mysql
+if [ $? -ne 0 ]
+then
+echo "MySQL is not installed...going to install"
+dnf install mysql -y
+if [ $? -ne 0]
+then 
+echo "package is not install"
+exit 1
+else
+echo "package is install"
+fi
+else
+echo "MySQL is already installed...Nothing to do"
 fi
