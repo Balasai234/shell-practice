de41e15187ffa8d9960b461b0490998c320a047c
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-05-18 14:59:14 +0530

    Arrays

--- Array.sh
+++ Array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 Array=("Sai","Varaprasad","Cheni")
-echo "The name in the array-list: $Array[0]"
-echo "The second name in the array-list : ${Array[1]}
+echo "The name in the array-list: ${Array[0]}"
+echo "The second name in the array-list : ${Array[1]}"
