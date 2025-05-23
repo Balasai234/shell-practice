ed19483ba09a844f4d876c47effcf8f415166748
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-05-23 22:37:13 +0530

    loops

--- loop.sh
+++ loop.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-red="\e[30m"
-blue="\e[32m"
-green="\e[0m"
-
-echo -e "$blue Hello World"
-echo  -e "$green Hello World"
+R="\e[30m"
+G="\e[31m"
+Y="\e[32m"
+N="\e[0
+UserId= $(id -u)
+if [ $UserId ne 0 ]
+then 
+echo -e "$R you don't have a root access"
+else
+echo -e "$G you are running with root access, continue.."
+fi
