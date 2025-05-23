92488b4a06ff3a81b329df02af9fa38281c1d6ea
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-05-23 22:43:51 +0530

    colours

--- loop.sh
+++ loop.sh
@@ -3,9 +3,9 @@
 R="\e[30m"
 G="\e[31m"
 Y="\e[32m"
-N="\e[0
+N="\e[0m
 UserId= $(id -u)
-if [ $UserId ne 0 ]
+if [ $UserId -ne 0 ]
 then 
 echo -e "$R ERROR: you don't have a root access $N"
 exit 1
