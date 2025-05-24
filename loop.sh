3e61dd9ed59be2132eeae6755c56d5f524d8424b
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-05-24 08:32:27 +0530

    loop

--- loop.sh
+++ loop.sh
@@ -8,26 +8,36 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
-echo -e "$R Please run with Root access $N"
-exit 1
-else
-echo -e "$G Please install the packages as you have root access $N"
+echo -e "$Rv ERROR:: Please run this script with root access $N"
+exit 1 #give other than 0 upto 127
+echo  "You are running with root access"
 fi
+
+#validate functions takes input as exit status, what command they tried to install
 Validate(){
-    if [ $1 -ne 0 ]
+    if [ $1 -eq 0 ]
     then
-    echo -e "$2 package is not installed $N"
-    exit 1
+    echo -e " Installing $2 is ... $G SUCCESS $N"
     else
-    echo -e "$1 package is installed"
+    echo -e "Installing $2 is ... $N"
+    exit 1
     fi
 }
 dnf list installed mysql
 if [ $? -ne 0 ]
 then 
-echo -e "$R package not found please install my sql if you required $N"
+echo "MySQL is not installed ... going to install it $N"
 dnf install mysql -y
 Validate $? "mySql"
 else
-echo -e "Nothing to install it you have installed $Gmysql package already$G"
+echo -e "Nothing to do MySQL... $Y already installed $N"
+fi
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+echo "Python is not installed ... $Ggoing to install it $N"
+dnf install python3 -y
+Validate $? "python3"
+else
+echo -e "Nothing to do Python3.... $Y already installed $N"
 fi
