f32444f7fdd983e161ff4104758ad4ccff2156c3
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-05-24 07:58:19 +0530

    loop

--- loop.sh
+++ loop.sh
@@ -6,9 +6,15 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
 echo -e "$R Please run with Root access $N"
+exit 1
 else
 echo -e "$G Please install the packages as you have root access $N"
 fi
+dnf list installed mysql
+if [$? -ne 0 ]
+then 
+echo -e "$R package not found please install my sql if you required $N"
+dnf install mysql -y
